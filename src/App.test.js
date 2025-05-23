3edff74f92842c31f31f43b31df70abf1de124d2
Chris Arsenault <christopher.arsenault@excella.com>
2025-05-23 00:18:23 -0700

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Tsonu/i);
+  const linkElement = screen.getAllByText(/Tsonu/i)[0];
   expect(linkElement).toBeInTheDocument();
 });
